92d884bb9d7fd461d3fc2fe089de8f70328277f5
aurelied1991 <aurelie.demange1991@gmail.com>
2025-03-03 18:48:03 +0100

    Phase 1 : Création des paquetages et classes, début de code de bddmanager

---
+++ habilitations2024/bddmanager/BddManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace habilitations2024.bddmanager
+{
+    public class BddManager
+    {
+        //propriete qui contient l'unique instance de la classe BddManager
+        private static BddManager instance = null;
+        //propriete qui permet de gerer une connexion MySql
+        private readonly MySqlConnection connection;
+
+        //constructeur de la classe qui recoit en paramètre une chaîne de connexion
+        private BddManager(string StringConnect)
+        {
+            //affecte une instance de la classe à la propriété privée connection puis connexion à la bdd
+            connection = new MySqlConnection(StringConnect);
+            connection.Open();
+        }
+
+        //Méthode GetInstance qui permet de créer une instance de la classe uniquement si la classe n'a pas encore d'instance
+        public static BddManager GetInstance(string StringConnect)
+        {
+            if (instance == null)
+            {
+                instance = new BddManager(StringConnect);
+            }
+            //retourne soit l'instance créée, soit l'instance déjà existante
+            return instance;
+        }
+
+        /// <summary>
+        /// Méthode qui exécute une requête autre que select
+        /// </summary>
+        /// <param name="StringQuery"> Requête SQL </param>
+        /// <param name="parameters"> Dictionnaire avec les évenuels paramètres de la requête</param>
+        public void reqUpdate(string StringQuery, Dictionary<string, object> parameters = null)
+        {
+            MySqlCommand command = new MySqlCommand(StringQuery, connection);
+            if (!(parameters is null))
+            {
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                }
+            }
+            command.Prepare();
+            command.ExecuteNonQuery();
+        }
+    }
+}   
---
+++ habilitations2024/view/FrmHabilitations.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Habilitations2024.view
+{
+    partial class FrmHabilitations
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNomDeveloppeur = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstNomDeveloppeur
+            // 
+            this.lstNomDeveloppeur.FormattingEnabled = true;
+            this.lstNomDeveloppeur.ItemHeight = 16;
+            this.lstNomDeveloppeur.Location = new System.Drawing.Point(97, 165);
+            this.lstNomDeveloppeur.Name = "lstNomDeveloppeur";
+            this.lstNomDeveloppeur.Size = new System.Drawing.Size(120, 84);
+            this.lstNomDeveloppeur.TabIndex = 0;
+            this.lstNomDeveloppeur.SelectedIndexChanged += new System.EventHandler(this.nomDeveloppeur_SelectedIndexChanged);
+            // 
+            // FrmHabilitations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstNomDeveloppeur);
+            this.Name = "FrmHabilitations";
+            this.Text = "Habilitations2024";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNomDeveloppeur;
+    }
+}
+
